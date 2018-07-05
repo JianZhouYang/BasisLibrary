b3c198c03d54045b238a9f08b52979c34b656937
JianZhouYang <yjz850314@gmail.com>
2018-07-05 23:25:08 +0800

    添加delete()删除文件方法。

--- kotlinutillibrary/src/main/java/com/yjz/kotlin/util/FileUtils.kt
+++ kotlinutillibrary/src/main/java/com/yjz/kotlin/util/FileUtils.kt
@@ -9,6 +9,42 @@ import java.io.File
  */
 object FileUtils {
 
+
+    /**
+     * 删除文件
+     * @return true: 删除成功；false: 删除失败
+     */
+    public fun delete(filePath: String?): Boolean{
+        return if (!filePath.isNullOrEmpty()) {
+            delete(File(filePath));
+        } else {
+            false;
+        }
+    }
+
+    /**
+     * 删除文件
+     * @return true: 删除成功；false: 删除失败
+     */
+    public fun delete(file: File?): Boolean{
+        return if (null != file && file.exists()) {
+            if (!file.isDirectory) {
+                file.delete();
+            } else {
+                val files = file.listFiles();
+                if (null != files) {
+                    for (i in files.indices) {
+                        delete(files[i]);
+                    }
+                }
+                file.delete();
+            }
+        } else {
+            false;
+        }
+    }
+
+
     /**
      * 文件目录是否存在
      * @return true:存在 false:不存在
